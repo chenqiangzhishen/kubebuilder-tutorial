e3f54df514a3cb7ada930dc91942d3b30bdb837e
QiangChen <chenqiangzhishen@163.com>
2020-06-07 21:06:11 +0800

    3. kubebuilder create api --group ship --version v1beta1 --kind Frigate
    Signed-off-by: QiangChen <chenqiangzhishen@163.com>

--- main.go
+++ main.go
@@ -20,6 +20,8 @@ import (
 	"flag"
 	"os"
 
+	shipv1beta1 "example.com/m/api/v1beta1"
+	"example.com/m/controllers"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
@@ -36,6 +38,7 @@ var (
 func init() {
 	_ = clientgoscheme.AddToScheme(scheme)
 
+	_ = shipv1beta1.AddToScheme(scheme)
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -62,6 +65,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.FrigateReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Frigate"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Frigate")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
